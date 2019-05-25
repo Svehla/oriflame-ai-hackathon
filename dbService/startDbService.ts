574ee76a9b9cd38150fd2a088b03f4f90df3f699
Josef Hornych <hornych@engeto.com>
2019-05-25 21:16:15 +0200

    move dbconfig to node config

--- dbService/startDbService.ts
+++ dbService/startDbService.ts
@@ -1,19 +1,26 @@
+import config from "config";
+import {Request, ConnectionPool, default as sql} from "mssql";
 
-import * as sql from "mssql"
-import sqlConfig from "../dbConfig"
+declare module "mssql" {
+  export const connect: any;
+}
 
 const startDbService = () => {
-  sql.connect(sqlConfig, error => {
+  sql.connect(config.get("DATABASE"), (error: Error) => {
     if (error) {
       // response.send(error);
     }
 
-    const request = new sql.Request();
+    const request = new Request();
 
     request.query("SELECT TOP(10) * FROM [dbo].CUSTOMERS", function(
       error,
       data
     ) {
+      if (!data) {
+        return;
+      }
+
       console.log(data.recordset[0])
       // if (error) response.send(error);
       // response.send(data);
